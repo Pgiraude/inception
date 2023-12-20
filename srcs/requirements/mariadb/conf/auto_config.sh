c31cc1977e26c50f50b58af1954f6016e92e6f99
Pierre GIRAUDEAU <pgiraude@made-f0Dr11s18.clusters.42paris.fr>
2023-12-20 19:07:30 +0100

    small modif

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 service mysql start
-export SQL_DATABASE=DB_Name
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;"
 mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;"
