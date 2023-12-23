b7f8eb7a730564bbc38324960fc8a4f43b52a5a2
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-12-23 12:52:11 +0100

    test config mariadb

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+export SQL_DATABASE=WP_database && \
+export SQL_USER=Pierre && \
+export SQL_PASSWORD=1 && \
+export SQL_ROOT_PASSWORD=
+
 service mysql start
+sleep 5
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;"
 mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;"
