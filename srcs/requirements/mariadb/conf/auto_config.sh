fadc722051bfcb8f9be14915eb5c9aad8376b0ea
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-12-23 15:35:13 +0100

    check wordpress

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -6,9 +6,10 @@ export SQL_PASSWORD=1 && \
 export SQL_ROOT_PASSWORD=
 
 service mysql start
-sleep 5
+sleep 3
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;"
 mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' ;"
 mysql -e "FLUSH PRIVILEGES;"
+
