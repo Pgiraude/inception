773553c8504e96ead6f8e192d7093810e1db6360
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-12-26 13:11:21 +0100

    wordpress=ok

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -9,5 +9,3 @@ else
     && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root \
     && wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --porcelain --allow-root
 fi
-
-/usr/sbin/php-fpm7.3 -F -R
