f97aa99bd813aa628efacf51f6fd2df430dc13fb
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-12-28 22:46:48 +0100

    still pb communication mariadb and wordpress

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 service mysql start
+#kill $(cat /var/run/mysqld/mysqld.pid)
+mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}' ;"
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' ;"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
