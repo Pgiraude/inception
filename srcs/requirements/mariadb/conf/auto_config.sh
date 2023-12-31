2026ca3739112eac64a2a14855f4b41e3b834247
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-12-31 14:44:33 +0100

    test

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -2,10 +2,11 @@
 
 service mysql start
 #kill $(cat /var/run/mysqld/mysqld.pid)
+sleep 2
 mysql -u root -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-kill $(cat /var/run/mysqld/mysqld.pid)
+#kill $(cat /var/run/mysqld/mysqld.pid)
 
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -4,14 +4,14 @@ if [ -f ./wp-config.php ]
 then
     echo "Wordpress already installed."
 else
-    cd var/www/wordpress \
-	&& sed -i "s/votre_nom_de_bdd/$SQL_DATABASE/g" wp-config-sample.php \
-    && sed -i "s/votre_utilisateur_de_bdd/$SQL_USER/g" wp-config-sample.php \
-	&& sed -i "s/votre_mdp_de_bdd/$SQL_PASSWORD/g" wp-config-sample.php \
-	&& sed -i "s/'DB_HOST', ''/'DB_HOST', '$SQL_HOSTNAME'/g" wp-config-sample.php \
-	&& cp wp-config-sample.php wp-config.php
-	#cd var/www/wordpress \
-    #&& wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$SQL_HOSTNAME --dbprefix=wp_ \
-    #&& wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root \
-    #&& wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --porcelain --allow-root
+    #cd var/www/wordpress \
+	#&& sed -i "s/votre_nom_de_bdd/$SQL_DATABASE/g" wp-config-sample.php \
+    #&& sed -i "s/votre_utilisateur_de_bdd/$SQL_USER/g" wp-config-sample.php \
+	#&& sed -i "s/votre_mdp_de_bdd/$SQL_PASSWORD/g" wp-config-sample.php \
+	#&& sed -i "s/'DB_HOST', ''/'DB_HOST', '$SQL_HOSTNAME'/g" wp-config-sample.php \
+	#&& cp wp-config-sample.php wp-config.php
+	cd var/www/wordpress \
+    && wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$SQL_HOSTNAME --dbprefix=wp_ \
+    && wp core install --url=https://localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root \
+    && wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --porcelain --allow-root
 fi
